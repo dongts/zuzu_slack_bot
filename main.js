97b694f8460fae1116ca0d2c4fe5a5d4842056d0
Dong Tran <dongts1201@gmail.com>
2017-02-03 14:57:36 +0700

    code to handle heroku

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var express = require('express');
-var app = express();
+var webserver = express();
+
+webserver.enable('trust proxy'); // For using with Heroku
+webserver.use(limiter);
+webserver.use(bodyParser.json());
+webserver.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
    res.send('Hello World');
