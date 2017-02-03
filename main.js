1f365c771ce105f73272e0d9405c3e1ec3c8d022
Dong Tran <dongts1201@gmail.com>
2017-02-03 15:32:16 +0700

    slack slash command hello world

--- main.js
+++ main.js
@@ -1,19 +1,91 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const Botkit = requrie('botkit');
 
-var webserver = express();
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
+    console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
+    process.exit(1);
+}
+
+var config = {}
+if (process.env.MONGODB_URI) {
+    var BotkitStorage = require('botkit-storage-mongo');
+    config = {
+        storage: BotkitStorage({mongoUri: process.env.MONGODB_URI}),
+    };
+} else {
+    config = {
+        json_file_store: './db_slackbutton_slash_command/',
+    };
+}
+
+var controller = Botkit.slackbot(config).configureSlackApp(
+    {
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        scopes: ['commands'],
+    }
+);
+
+controller.setupWebserver(process.env.PORT, function (err, webserver) {
+    controller.createWebhookEndpoints(controller.webserver);
+
+    controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
+        if (err) {
+            res.status(500).send('ERROR: ' + err);
+        } else {
+            res.send('Success!');
+        }
+    });
+});
+
+// var webserver = express();
 
 // webserver.enable('trust proxy'); // For using with Heroku
 // webserver.use(bodyParser.json());
 // webserver.use(bodyParser.urlencoded({ extended: true }));
 
-webserver.get('/', function (req, res) {
-   res.send('Hello World');
-})
+// webserver.get('/', function (req, res) {
+//    res.send('Hello World');
+// })
 
-var server = webserver.listen(process.env.PORT || 8080, function () {
-   var host = server.address().address
-   var port = server.address().port
+// var server = webserver.listen(process.env.PORT || 8080, function () {
+//    var host = server.address().address
+//    var port = server.address().port
 
-   console.log("Example app listening at http://%s:%s", host, port)
+//    console.log("Example app listening at http://%s:%s", host, port)
+// })
+
+
+controller.on('slash_command', function (slashCommand, message) {
+
+    switch (message.command) {
+        case "/echo": //handle the `/echo` slash command. We might have others assigned to this app too!
+            // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
+            // Otherwise just echo back to them what they sent us.
+
+            // but first, let's make sure the token matches!
+            if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
+
+            // if no text was supplied, treat it as a help command
+            if (message.text === "" || message.text === "help") {
+                slashCommand.replyPrivate(message,
+                    "I echo back what you tell me. " +
+                    "Try typing `/echo hello` to see.");
+                return;
+            }
+
+            // If we made it here, just echo what the user typed back at them
+            //TODO You do it!
+            slashCommand.replyPublic(message, "1", function() {
+                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+            });
+
+            break;
+        default:
+            slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
+
+    }
+
 })
+;
